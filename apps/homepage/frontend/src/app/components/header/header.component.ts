94883be67c5842de36c629cb67195eba841c1293
Stefan Theissl <stefan.theissl@gmail.com>
2022-03-29 23:05:53 +0200

    Make header menu responsive

--- apps/homepage/frontend/src/app/components/header/header.component.ts
+++ apps/homepage/frontend/src/app/components/header/header.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+    showMobileMenu = false;
+
+    toggleMobileMenu() {
+        this.showMobileMenu = !this.showMobileMenu;
+    }
+}
