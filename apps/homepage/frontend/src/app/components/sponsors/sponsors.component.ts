d9ff9ead1b9c2283ba0b7bd89ad36f9cfbcf8406
Stefan Theissl <stefan.theissl@gmail.com>
2022-04-04 16:53:12 +0200

    Improvement to sponsor slider

--- apps/homepage/frontend/src/app/components/sponsors/sponsors.component.ts
+++ apps/homepage/frontend/src/app/components/sponsors/sponsors.component.ts
@@ -11,11 +11,24 @@ export class SponsorsComponent implements OnInit {
 
     sponsors: Sponsor[] = [];
 
+    SPONSOR_CONTAINER_ID = 'sponsor_container_element';
+
     constructor(private apiService: ApiService) { }
 
     ngOnInit(): void {
         this.apiService.getSponsorenFromApi()
-            .then(list => this.sponsors = list);
+            .then(list => {
+                this.sponsors = list;
+                this.calcSponsorContainerAnimationDuration();
+            });
+    }
+
+    calcSponsorContainerAnimationDuration() {
+        const sponsorContainerElement = document.getElementById(this.SPONSOR_CONTAINER_ID);
+
+        if (sponsorContainerElement) {
+            sponsorContainerElement.style.animationDuration = this.sponsors.length * 2 + 's';
+        }
     }
 
 }
