bbeb419450ba12a77ace788cf190a772b17a85bd
Stefan Theissl <stefan.theissl@gmail.com>
2023-06-19 16:48:14 +0200

    fetch team data in backend from cockpit

--- apps/backend/api/src/generalplan/index.php
+++ apps/backend/api/src/generalplan/index.php
@@ -1,29 +1,23 @@
 <?php
-// $teams = json_decode(file_get_contents("./teams.json"));
-
 require "../helpers/mysqlHandler.php";
 
 set_error_handler("warning_handler", E_WARNING);
 
-function warning_handler($errno, $errstr) { 
+function warning_handler($errno, $errstr)
+{
     header("HTTP/1.1 500 Internal Server Error");
     die('An Unexpected Error occurred');
 }
 
-// Read from DB
-$sql = "SELECT * FROM `teams`;";
-    
-$teams = json_decode(selectFromDB($sql));
+$result = file_get_contents("https://fcschwarzach.com/cockpit/api/collections/get/spielplan?token=ee267dda1fbfde50ed854fb6af19ca");
+$teams = json_decode($result)->{"entries"};
 
 foreach ($teams as $team) {
     $url = str_replace('webcal://', 'https://', $team->{"url"});
     $team->{"data"} = file_get_contents($url);
-    // $team->{"data"} = $url;
 };
-
 
 
 // RETURN VALUES
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($teams);
-
