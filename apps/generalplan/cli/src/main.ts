0214fc5d1e065f47411bb28458e49ae4f368c0fb
Stefan Theissl <stefan.theissl@gmail.com>
2024-03-16 13:56:38 +0100

    update start and end date

--- apps/generalplan/cli/src/main.ts
+++ apps/generalplan/cli/src/main.ts
@@ -4,9 +4,9 @@ import { createSimpleHomePlan } from "./app/createSimpleHomePlan";
 import { AsyncWait } from "./app/helpers/asyncWait";
 import { fetchTeamDataFromApi } from "./app/helpers/fetchFromWebcal";
 
-const startDate = new Date('2023-07-01');
+const startDate = new Date('2024-01-01');
 startDate.setHours(0, 0, 0, 0);
-const endDate = new Date('2023-12-30');
+const endDate = new Date('2024-06-30');
 const outputPath = path.join('dist', 'apps', 'generalplan');
 
 const asyncWait = new AsyncWait();
